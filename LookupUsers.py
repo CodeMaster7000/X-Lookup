b81ea6b7b94ae44b094845e3ee51f2562bbd2f1f
CodeMaster7000 <CodeMaster7K@gmail.com>
2024-07-23 22:18:15 +0100

    Upload LookupUsers.py

---
+++ LookupUsers.py
@@ -0,0 +1,18 @@
+import tweepy
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+user_input = input("Enter ID(s) separated by commas: ")
+user_ids = [int(uid.strip()) for uid in user_input.split(",")]
+users = api.lookup_users(user_ids)
+
+for user in users:
+    print("ID: " + str(user.id))
+    print("Screen Name: " + user.screen_name, end="\n\n")
